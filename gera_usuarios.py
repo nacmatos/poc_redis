a239d6be4cc4ab438d35c8d1b2720fc73710d47c
Nereu Matos <nereu.matos@infoglobo.com.br>
2026-02-11 15:01:02 -0300

    Fixando uma versão máxima para o mimesis

--- gera_usuarios.py
+++ gera_usuarios.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
+import random
 import sys
 
 import mimesis
 import mimesis.builtins
+from mimesis.locales import Locale
 
-p = mimesis.Person(mimesis.Locale.PT_BR)
+p = mimesis.Person(locale=mimesis.Locale.PT_BR)
 d = mimesis.Datetime()
 p_br = mimesis.builtins.BrazilSpecProvider()
+age = lambda: int(random.gauss(37,14))
 
 print('cpf,nome,email,idade,ultacesso');
-for i in range(0,int(sys.argv[1])): print(f'{p_br.cpf()},{p.full_name()},{p.email()},{p.age()},{d.date()}', flush=True)
+for i in range(0,int(sys.argv[1])): print(f'{p_br.cpf()},{p.full_name()},{p.email()},{age()},{d.date()}', flush=True)
